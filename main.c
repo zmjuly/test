afb48a661df30fbfd029f44a57799313552011d2
zmjuly <1301505453@qq.com>
2019-04-22 16:52:14 +0800

    123

---
+++ main.c
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>爱淘宝</title>
+	<style type="text/css">
+	table tr td {border:1px solid #000;}
+	.img1{
+		margin-left: 133px;
+	}
+	.div{
+		margin: 3px 130px 3px 130px;
+	}
+	.img2{
+		padding: 30px;
+
+	</style>
+<body>
+<img class="img1" src="./01.jpg" style="width:1190px; height:90px">
+<div class="div">
+	<table>
+		<tr>
+			<td style="width:300px; height:90px">
+			<img src=".png" style="width:150px; height:30px"></br>
+			<img class="img2" src="./TB1drskIpXXXXbQXFXX81rLGFXX-163-42.png" style="width:163px; height:40px">
+			</td>
+			<td style="width:650px; height:80px">
+			</td>
+			<td style="width:300px; height:80px">
+			<img class="img2" src="./1.jpg" style="width:250px; height:80px">
+			</td>
+		</tr>
+	</table>
+</div>
+
+<div class="img1">
+	<table>
+		
+		<tr>
+			<td style="width:40px;height:50px" rowspan="6">
+			<img src="./EJFZA[7H]BA5ECUYJXT[]YA.jpg">
+			<td style="width:210px;height:50px"></td>
+		</tr>
+		<tr>
+			
+			<td style="width:210px;height:50px"></td>
+		</tr>
+        <tr>
+			
+			<td style="width:210px;height:50px"></td>
+		</tr>
+		<tr>
+			
+			<td style="width:210px;height:50px"></td>
+		</tr>
+		<tr>
+			
+			<td style="width:210px;height:50px"></td>
+		</tr>
+		<tr>
+			
+			<td style="width:210px;height:50px"></td>
+		</tr>
+	</table>
+</div>
+
+
+
+</body>
+<html>
